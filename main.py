354df06295a03f585ecd99e98a0ec39b6fe6411f
baumiajoint <marcelb1210@gmail.com>
2024-11-30 15:08:49 +0100

    count characters update

--- main.py
+++ main.py
@@ -2,7 +2,16 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document")
+    characters = get_num_characters(text)
+    print(characters)
+
+
+def get_num_characters(text):
+    characters = {}
+    for char in text.lower():
+        count = characters.get(char, 0) + 1
+        characters[char] = count
+    return characters
 
 
 def get_num_words(text):
